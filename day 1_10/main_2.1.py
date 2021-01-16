56943ac7bc8343c5de9c5778da78c6cca36a4cc6
peefer66 <peefer1966@gmail.com>
2021-01-16 13:26:20 +0000

    2.1

---
+++ day 1_10/main_2.1.py
@@ -0,0 +1,11 @@
+# Write a program that adds the digits in a 2 digit number.
+# e.g. if the input was 35, then the output should be 3 + 5 = 8
+# Warning. Do not change the code on lines 1-3. Your program should 
+# work for different inputs. e.g. any two-digit number.
+
+num = input('Please enter a two digit number: ')
+#num0 = int(num[0])
+#num1 = int(num[1])
+
+print(int(num[0]) + int(num[1]))
+
