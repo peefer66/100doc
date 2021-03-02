363e074051cab5af53309a59d51e5f2de5e874de
peefer66 <peefer66@hotmail.com>
2021-03-02 22:01:03 +0000

    Start coffee maker

---
+++ day 15_20/coffee maker/coffee_maker.py
@@ -0,0 +1,18 @@
+
+
+def main():
+    menu = {
+        
+        'esspresso':{'ingredients': {'water':0,'coffee':100 }, 'cost':2.0},
+
+        'latte':{'ingredients':{'water':100, 'milk':100,'coffee':100}, 'cost':2.5},
+        
+        'cappuccino':{'ingredients':{'water':100, 'milk':100,'coffee':100}, 'cost':3.0}
+
+    }
+    
+
+main()
+
+
+
