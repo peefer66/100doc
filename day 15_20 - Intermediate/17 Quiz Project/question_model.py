df510253a2a95620cd3310419cef4a6cd3a73318
peefer66 <peefer66@hotmail.com>
2021-03-17 16:28:26 +0000

    Finished Quiz project

--- day 15_20 - Intermediate/17 Quiz Project/question_model.py
+++ day 15_20 - Intermediate/17 Quiz Project/question_model.py
@@ -1,5 +1,5 @@
 class Question():
     def __init__(self,q_text,q_answer):
-        self.q_text = q_text
-        self.q_answer = q_answer
+        self.text = q_text
+        self.answer = q_answer
         
--- day 15_20 - Intermediate/17 Quiz Project/quiz_brain.py
+++ day 15_20 - Intermediate/17 Quiz Project/quiz_brain.py
@@ -1,22 +1,29 @@
 class QuizBrain:
+
     def __init__(self,q_list):
         self.q_number = 0
         self.q_score = 0
         self.q_list = q_list
 
+    def check_answer(self, user_answer, correct_answer):
+        '''Compares the users answer with the ccorrect answer '''
+        if user_answer.lower() == correct_answer.lower():
+            self.q_score += 1
+            print('Correct')
+        else:
+            print('Incorrect')
+            print(f'The correct answer was {correct_answer}') 
+        print(f'Your current score is : {self.q_score}/{self.q_number}')      
+
 #TODO Ask the question
     def next_question(self):
         current_question = self.q_list[self.q_number]
         self.q_number += 1
-        user_answer = input(f'Q.{self.q_number} True or False. {current_question.q_text}')
-        check_answer(user_answer,current_question.answer)
+        user_answer = input(f'Q.{self.q_number} True or False. {current_question.text}')
+        self.check_answer(user_answer,current_question.answer)
+        
 
- 
-#TODO Check if the answer is correct or not
-    def check_answer(self,user_answer,correct_answer):
-        pass
-
-
+   
 #TODO Check if we are at the end of the list
     def still_has_questions(self):
         return self.q_number < len(self.q_list)
